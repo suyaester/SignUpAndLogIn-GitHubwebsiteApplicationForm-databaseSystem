7e7bd669eaf88a5969d40972488ae877cf408591
suyaester <suyaester12@gmail.com>
2024-12-13 21:45:44 +0800

    Add files via upload

---
+++ LoginAndSignUpESuya/src/loginandsignupesuya/LoginAndSignUpESuya.java
@@ -0,0 +1,16 @@
+
+package loginandsignupesuya;
+
+ 
+public class LoginAndSignUpESuya {
+
+    public static void main(String[] args) {
+
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null); 
+    }
+    
+}
+
